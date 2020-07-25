e1885c96fffec089e4d721d0064ace9d110ecebe
arvindh.cm <arvindh.cm@zohocorp.com>
2020-07-25 22:40:14 +0530

    [SAFARI BROWSER SUPPORT]

--- src/index.js
+++ src/index.js
@@ -95,14 +95,14 @@ console.log(activeTab)
 
       <div className={`layout ` + (isDesktop? 'container':'') }>
 
-<div className={`navigation navbar py-3 px-4 border-top`}>
-          <ul className={`nav navbar-nav flex-row flex-lg-column flex-grow-1 justify-content-between justify-content-lg-center root-nav`} >
+<div className={`navigation px-4 border-top`}>
+          <ul className={`nav navbar-nav flex-row flex-lg-column flex-grow-1 justify-content-between justify-content-lg-center py-2 root-nav`} >
 
-            <li><a className={this.state.activeTab == 2? 'active':''}>  <Edit onClick={e => this.switchTab(e,2) }/>  </a></li>
-            <li className={`mt-lg-5`}>  <a className={this.state.activeTab == 3? 'active':''}>  <Users onClick={e => this.switchTab(e,3) }/>  </a> </li>
-            <li className={`mt-lg-5`}> <a className={this.state.activeTab == 1? 'active':''}>  <MessageSquare onClick={ e => this.switchTab(e,1)} />  </a> </li>
-            <li className={`mt-lg-5`}> <a className={this.state.activeTab == 4? 'active':''}>  <User onClick={e => this.switchTab(e,4) }/>  </a> </li>
-            <li className={`mt-lg-5`}>  <a className={this.state.activeTabForMain ==2 && this.state.mainOpen? 'active':''}>  <Settings onClick={e => this.switchTabForMain(e,2) }/>  </a> </li>
+            <li className={this.state.activeTab == 2? 'active':''}><a >  <Edit onClick={e => this.switchTab(e,2) }/>  </a></li>
+            <li className={`mt-lg-5 `+(this.state.activeTab == 3? 'active':'')}>  <a>  <Users onClick={e => this.switchTab(e,3) }/>  </a> </li>
+            <li className={`mt-lg-5 `+(this.state.activeTab == 1? 'active':'')}> <a>  <MessageSquare onClick={ e => this.switchTab(e,1)} />  </a> </li>
+            <li className={`mt-lg-5 `+(this.state.activeTab == 4? 'active':'')}> <a >  <User onClick={e => this.switchTab(e,4) }/>  </a> </li>
+            <li className={`mt-lg-5 `+(this.state.activeTabForMain == 2 && this.state.mainOpen? 'active':'')}> <a >  <Settings onClick={e => this.switchTabForMain(e,2) }/>  </a> </li>
 
           </ul>
 
